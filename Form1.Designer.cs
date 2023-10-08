f2b08eea3fae270044f3a6b1846e016a6372355c
zyont <zyont@MSI>
2023-10-08 15:31:57 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace LotteryNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFloridaLotto = new System.Windows.Forms.Label();
+            this.lblPowerBall = new System.Windows.Forms.Label();
+            this.txtFloridaLotto1 = new System.Windows.Forms.TextBox();
+            this.txtFloridaLotto2 = new System.Windows.Forms.TextBox();
+            this.txtFloridaLotto3 = new System.Windows.Forms.TextBox();
+            this.txtFloridaLotto4 = new System.Windows.Forms.TextBox();
+            this.txtFloridaLotto5 = new System.Windows.Forms.TextBox();
+            this.txtFloridaLotto6 = new System.Windows.Forms.TextBox();
+            this.txtPowerBallBonus = new System.Windows.Forms.TextBox();
+            this.txtPowerBall5 = new System.Windows.Forms.TextBox();
+            this.txtPowerBall4 = new System.Windows.Forms.TextBox();
+            this.txtPowerBall3 = new System.Windows.Forms.TextBox();
+            this.txtPowerBall2 = new System.Windows.Forms.TextBox();
+            this.txtPowerBall1 = new System.Windows.Forms.TextBox();
+            this.btnFloridaLotto = new System.Windows.Forms.Button();
+            this.btnPowerBall = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtMegaBonus = new System.Windows.Forms.TextBox();
+            this.txtMega5 = new System.Windows.Forms.TextBox();
+            this.txtMega4 = new System.Windows.Forms.TextBox();
+            this.txtMega3 = new System.Windows.Forms.TextBox();
+            this.txtMega2 = new System.Windows.Forms.TextBox();
+            this.txtMega1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMega = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFloridaLotto
+            // 
+            this.lblFloridaLotto.AutoSize = true;
+            this.lblFloridaLotto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblFloridaLotto.Location = new System.Drawing.Point(71, 69);
+            this.lblFloridaLotto.Name = "lblFloridaLotto";
+            this.lblFloridaLotto.Size = new System.Drawing.Size(125, 25);
+            this.lblFloridaLotto.TabIndex = 0;
+            this.lblFloridaLotto.Text = "Florida Lotto:";
+            // 
+            // lblPowerBall
+            // 
+            this.lblPowerBall.AutoSize = true;
+            this.lblPowerBall.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblPowerBall.Location = new System.Drawing.Point(71, 177);
+            this.lblPowerBall.Name = "lblPowerBall";
+            this.lblPowerBall.Size = new System.Drawing.Size(110, 25);
+            this.lblPowerBall.TabIndex = 1;
+            this.lblPowerBall.Text = "Power Ball:";
+            // 
+            // txtFloridaLotto1
+            // 
+            this.txtFloridaLotto1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto1.Location = new System.Drawing.Point(227, 69);
+            this.txtFloridaLotto1.Name = "txtFloridaLotto1";
+            this.txtFloridaLotto1.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto1.TabIndex = 2;
+            // 
+            // txtFloridaLotto2
+            // 
+            this.txtFloridaLotto2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto2.Location = new System.Drawing.Point(304, 69);
+            this.txtFloridaLotto2.Name = "txtFloridaLotto2";
+            this.txtFloridaLotto2.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto2.TabIndex = 3;
+            // 
+            // txtFloridaLotto3
+            // 
+            this.txtFloridaLotto3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto3.Location = new System.Drawing.Point(378, 69);
+            this.txtFloridaLotto3.Name = "txtFloridaLotto3";
+            this.txtFloridaLotto3.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto3.TabIndex = 4;
+            // 
+            // txtFloridaLotto4
+            // 
+            this.txtFloridaLotto4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto4.Location = new System.Drawing.Point(452, 69);
+            this.txtFloridaLotto4.Name = "txtFloridaLotto4";
+            this.txtFloridaLotto4.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto4.TabIndex = 5;
+            // 
+            // txtFloridaLotto5
+            // 
+            this.txtFloridaLotto5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto5.Location = new System.Drawing.Point(528, 69);
+            this.txtFloridaLotto5.Name = "txtFloridaLotto5";
+            this.txtFloridaLotto5.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto5.TabIndex = 6;
+            // 
+            // txtFloridaLotto6
+            // 
+            this.txtFloridaLotto6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtFloridaLotto6.Location = new System.Drawing.Point(605, 69);
+            this.txtFloridaLotto6.Name = "txtFloridaLotto6";
+            this.txtFloridaLotto6.Size = new System.Drawing.Size(57, 30);
+            this.txtFloridaLotto6.TabIndex = 7;
+            // 
+            // txtPowerBallBonus
+            // 
+            this.txtPowerBallBonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBallBonus.Location = new System.Drawing.Point(605, 174);
+            this.txtPowerBallBonus.Name = "txtPowerBallBonus";
+            this.txtPowerBallBonus.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBallBonus.TabIndex = 13;
+            // 
+            // txtPowerBall5
+            // 
+            this.txtPowerBall5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBall5.Location = new System.Drawing.Point(528, 174);
+            this.txtPowerBall5.Name = "txtPowerBall5";
+            this.txtPowerBall5.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBall5.TabIndex = 12;
+            // 
+            // txtPowerBall4
+            // 
+            this.txtPowerBall4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBall4.Location = new System.Drawing.Point(452, 174);
+            this.txtPowerBall4.Name = "txtPowerBall4";
+            this.txtPowerBall4.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBall4.TabIndex = 11;
+            // 
+            // txtPowerBall3
+            // 
+            this.txtPowerBall3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBall3.Location = new System.Drawing.Point(378, 174);
+            this.txtPowerBall3.Name = "txtPowerBall3";
+            this.txtPowerBall3.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBall3.TabIndex = 10;
+            // 
+            // txtPowerBall2
+            // 
+            this.txtPowerBall2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBall2.Location = new System.Drawing.Point(304, 174);
+            this.txtPowerBall2.Name = "txtPowerBall2";
+            this.txtPowerBall2.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBall2.TabIndex = 9;
+            // 
+            // txtPowerBall1
+            // 
+            this.txtPowerBall1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPowerBall1.Location = new System.Drawing.Point(227, 174);
+            this.txtPowerBall1.Name = "txtPowerBall1";
+            this.txtPowerBall1.Size = new System.Drawing.Size(57, 30);
+            this.txtPowerBall1.TabIndex = 8;
+            // 
+            // btnFloridaLotto
+            // 
+            this.btnFloridaLotto.Location = new System.Drawing.Point(112, 353);
+            this.btnFloridaLotto.Name = "btnFloridaLotto";
+            this.btnFloridaLotto.Size = new System.Drawing.Size(83, 46);
+            this.btnFloridaLotto.TabIndex = 14;
+            this.btnFloridaLotto.Text = "Florida Lotto";
+            this.btnFloridaLotto.UseVisualStyleBackColor = true;
+            this.btnFloridaLotto.Click += new System.EventHandler(this.btnFloridaLotto_Click);
+            // 
+            // btnPowerBall
+            // 
+            this.btnPowerBall.Location = new System.Drawing.Point(227, 353);
+            this.btnPowerBall.Name = "btnPowerBall";
+            this.btnPowerBall.Size = new System.Drawing.Size(83, 46);
+            this.btnPowerBall.TabIndex = 15;
+            this.btnPowerBall.Text = "Power Ball";
+            this.btnPowerBall.UseVisualStyleBackColor = true;
+            this.btnPowerBall.Click += new System.EventHandler(this.btnPowerBall_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(464, 353);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(83, 46);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(579, 353);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 46);
+            this.btnClose.TabIndex = 17;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtMegaBonus
+            // 
+            this.txtMegaBonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMegaBonus.Location = new System.Drawing.Point(605, 256);
+            this.txtMegaBonus.Name = "txtMegaBonus";
+            this.txtMegaBonus.Size = new System.Drawing.Size(57, 30);
+            this.txtMegaBonus.TabIndex = 24;
+            // 
+            // txtMega5
+            // 
+            this.txtMega5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMega5.Location = new System.Drawing.Point(528, 256);
+            this.txtMega5.Name = "txtMega5";
+            this.txtMega5.Size = new System.Drawing.Size(57, 30);
+            this.txtMega5.TabIndex = 23;
+            // 
+            // txtMega4
+            // 
+            this.txtMega4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMega4.Location = new System.Drawing.Point(452, 256);
+            this.txtMega4.Name = "txtMega4";
+            this.txtMega4.Size = new System.Drawing.Size(57, 30);
+            this.txtMega4.TabIndex = 22;
+            // 
+            // txtMega3
+            // 
+            this.txtMega3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMega3.Location = new System.Drawing.Point(378, 256);
+            this.txtMega3.Name = "txtMega3";
+            this.txtMega3.Size = new System.Drawing.Size(57, 30);
+            this.txtMega3.TabIndex = 21;
+            // 
+            // txtMega2
+            // 
+            this.txtMega2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMega2.Location = new System.Drawing.Point(304, 256);
+            this.txtMega2.Name = "txtMega2";
+            this.txtMega2.Size = new System.Drawing.Size(57, 30);
+            this.txtMega2.TabIndex = 20;
+            // 
+            // txtMega1
+            // 
+            this.txtMega1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMega1.Location = new System.Drawing.Point(227, 256);
+            this.txtMega1.Name = "txtMega1";
+            this.txtMega1.Size = new System.Drawing.Size(57, 30);
+            this.txtMega1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(71, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Mega Millions:";
+            // 
+            // btnMega
+            // 
+            this.btnMega.Location = new System.Drawing.Point(352, 353);
+            this.btnMega.Name = "btnMega";
+            this.btnMega.Size = new System.Drawing.Size(83, 46);
+            this.btnMega.TabIndex = 25;
+            this.btnMega.Text = "Mega Millions";
+            this.btnMega.UseVisualStyleBackColor = true;
+            this.btnMega.Click += new System.EventHandler(this.btnMega_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMega);
+            this.Controls.Add(this.txtMegaBonus);
+            this.Controls.Add(this.txtMega5);
+            this.Controls.Add(this.txtMega4);
+            this.Controls.Add(this.txtMega3);
+            this.Controls.Add(this.txtMega2);
+            this.Controls.Add(this.txtMega1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPowerBall);
+            this.Controls.Add(this.btnFloridaLotto);
+            this.Controls.Add(this.txtPowerBallBonus);
+            this.Controls.Add(this.txtPowerBall5);
+            this.Controls.Add(this.txtPowerBall4);
+            this.Controls.Add(this.txtPowerBall3);
+            this.Controls.Add(this.txtPowerBall2);
+            this.Controls.Add(this.txtPowerBall1);
+            this.Controls.Add(this.txtFloridaLotto6);
+            this.Controls.Add(this.txtFloridaLotto5);
+            this.Controls.Add(this.txtFloridaLotto4);
+            this.Controls.Add(this.txtFloridaLotto3);
+            this.Controls.Add(this.txtFloridaLotto2);
+            this.Controls.Add(this.txtFloridaLotto1);
+            this.Controls.Add(this.lblPowerBall);
+            this.Controls.Add(this.lblFloridaLotto);
+            this.Name = "Form1";
+            this.Text = "Lottery Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFloridaLotto;
+        private System.Windows.Forms.Label lblPowerBall;
+        private System.Windows.Forms.TextBox txtFloridaLotto1;
+        private System.Windows.Forms.TextBox txtFloridaLotto2;
+        private System.Windows.Forms.TextBox txtFloridaLotto3;
+        private System.Windows.Forms.TextBox txtFloridaLotto4;
+        private System.Windows.Forms.TextBox txtFloridaLotto5;
+        private System.Windows.Forms.TextBox txtFloridaLotto6;
+        private System.Windows.Forms.TextBox txtPowerBallBonus;
+        private System.Windows.Forms.TextBox txtPowerBall5;
+        private System.Windows.Forms.TextBox txtPowerBall4;
+        private System.Windows.Forms.TextBox txtPowerBall3;
+        private System.Windows.Forms.TextBox txtPowerBall2;
+        private System.Windows.Forms.TextBox txtPowerBall1;
+        private System.Windows.Forms.Button btnFloridaLotto;
+        private System.Windows.Forms.Button btnPowerBall;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtMegaBonus;
+        private System.Windows.Forms.TextBox txtMega5;
+        private System.Windows.Forms.TextBox txtMega4;
+        private System.Windows.Forms.TextBox txtMega3;
+        private System.Windows.Forms.TextBox txtMega2;
+        private System.Windows.Forms.TextBox txtMega1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMega;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtFloridaLotto1.Enabled = false;
+            txtFloridaLotto2.Enabled = false;
+            txtFloridaLotto3.Enabled = false;
+            txtFloridaLotto4.Enabled = false;
+            txtFloridaLotto5.Enabled = false;
+            txtFloridaLotto6.Enabled = false;
+            txtPowerBall1.Enabled = false;
+            txtPowerBall2.Enabled = false;
+            txtPowerBall3.Enabled = false;
+            txtPowerBall4.Enabled = false;
+            txtPowerBall5.Enabled = false;
+            txtPowerBallBonus.Enabled = false;
+            txtMega1.Enabled = false;
+            txtMega2.Enabled = false;
+            txtMega3.Enabled = false;
+            txtMega4.Enabled = false;
+            txtMega5.Enabled = false;
+            txtMegaBonus.Enabled = false;
+        }
+
+        private void btnFloridaLotto_Click(object sender, EventArgs e)
+        {
+            int FloridaLottoNumber1 = 0;
+            int FloridaLottoNumber2 = 0;
+            int FloridaLottoNumber3 = 0;
+            int FloridaLottoNumber4 = 0;
+            int FloridaLottoNumber5 = 0;
+            int FloridaLottoNumber6 = 0;
+            while ( FloridaLottoNumber1 == FloridaLottoNumber2 || FloridaLottoNumber1 == FloridaLottoNumber3 ||
+                    FloridaLottoNumber1 == FloridaLottoNumber4 || FloridaLottoNumber1 == FloridaLottoNumber5 ||
+                    FloridaLottoNumber1 == FloridaLottoNumber6 || FloridaLottoNumber2 == FloridaLottoNumber3 ||
+                    FloridaLottoNumber2 == FloridaLottoNumber4 || FloridaLottoNumber2 == FloridaLottoNumber5 ||
+                    FloridaLottoNumber2 == FloridaLottoNumber6 || FloridaLottoNumber3 == FloridaLottoNumber4 ||
+                    FloridaLottoNumber3 == FloridaLottoNumber5 || FloridaLottoNumber3 == FloridaLottoNumber6 ||
+                    FloridaLottoNumber4 == FloridaLottoNumber5 || FloridaLottoNumber4 == FloridaLottoNumber6 )
+            {
+                Random rand = new Random();
+                FloridaLottoNumber1 = rand.Next(1, 53);
+                FloridaLottoNumber2 = rand.Next(1, 53);
+                FloridaLottoNumber3 = rand.Next(1, 53);
+                FloridaLottoNumber4 = rand.Next(1, 53);
+                FloridaLottoNumber5 = rand.Next(1, 53);
+                FloridaLottoNumber6 = rand.Next(1, 53);
+            }
+            txtFloridaLotto1.Text = FloridaLottoNumber1.ToString();
+            txtFloridaLotto2.Text = FloridaLottoNumber2.ToString();
+            txtFloridaLotto3.Text = FloridaLottoNumber3.ToString();
+            txtFloridaLotto4.Text = FloridaLottoNumber4.ToString();
+            txtFloridaLotto5.Text = FloridaLottoNumber5.ToString();
+            txtFloridaLotto6.Text = FloridaLottoNumber6.ToString();
+        }
+
+        private void btnPowerBall_Click(object sender, EventArgs e)
+        {
+            int PowerBallNumber1 = 0;
+            int PowerBallNumber2 = 0;
+            int PowerBallNumber3 = 0;
+            int PowerBallNumber4 = 0;
+            int PowerBallNumber5 = 0;
+            int PowerBallNumberBonus = 0;
+            while ( PowerBallNumber1 == PowerBallNumber2 || PowerBallNumber1 == PowerBallNumber3 ||
+                    PowerBallNumber1 == PowerBallNumber4 || PowerBallNumber1 == PowerBallNumber5 ||
+                    PowerBallNumber2 == PowerBallNumber3 || PowerBallNumber2 == PowerBallNumber4 ||
+                    PowerBallNumber2 == PowerBallNumber5 || PowerBallNumber3 == PowerBallNumber4 ||
+                    PowerBallNumber3 == PowerBallNumber5 || PowerBallNumber4 == PowerBallNumber5 )
+            {
+                Random rand = new Random();
+                PowerBallNumber1 = rand.Next(1, 69);
+                PowerBallNumber2 = rand.Next(1, 69);
+                PowerBallNumber3 = rand.Next(1, 69);
+                PowerBallNumber4 = rand.Next(1, 69);
+                PowerBallNumber5 = rand.Next(1, 69);
+                PowerBallNumberBonus = rand.Next(1, 26);
+            }
+            txtPowerBall1.Text = PowerBallNumber1.ToString();
+            txtPowerBall2.Text = PowerBallNumber2.ToString();
+            txtPowerBall3.Text = PowerBallNumber3.ToString();
+            txtPowerBall4.Text = PowerBallNumber4.ToString();
+            txtPowerBall5.Text = PowerBallNumber5.ToString();
+            txtPowerBallBonus.Text = PowerBallNumberBonus.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFloridaLotto1.Clear();
+            txtFloridaLotto2.Clear();
+            txtFloridaLotto3.Clear();
+            txtFloridaLotto4.Clear();
+            txtFloridaLotto5.Clear();
+            txtFloridaLotto6.Clear();
+            txtPowerBall1.Clear();
+            txtPowerBall2.Clear();
+            txtPowerBall3.Clear();
+            txtPowerBall4.Clear();
+            txtPowerBall5.Clear();
+            txtPowerBallBonus.Clear();
+            txtMega1.Clear();
+            txtMega2.Clear();
+            txtMega3.Clear();
+            txtMega4.Clear();
+            txtMega5.Clear();
+            txtMegaBonus.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMega_Click(object sender, EventArgs e)
+        {
+            int MegaNumber1 = 0;
+            int MegaNumber2 = 0;
+            int MegaNumber3 = 0;
+            int MegaNumber4 = 0;
+            int MegaNumber5 = 0;
+            int MegaNumberBonus = 0;
+            while (MegaNumber1 == MegaNumber2 || MegaNumber1 == MegaNumber3 ||
+                    MegaNumber1 == MegaNumber4 || MegaNumber1 == MegaNumber5 ||
+                    MegaNumber2 == MegaNumber3 || MegaNumber2 == MegaNumber4 ||
+                    MegaNumber2 == MegaNumber5 || MegaNumber3 == MegaNumber4 ||
+                    MegaNumber3 == MegaNumber5 || MegaNumber4 == MegaNumber5)
+            {
+                Random rand = new Random();
+                MegaNumber1 = rand.Next(1, 70);
+                MegaNumber2 = rand.Next(1, 70);
+                MegaNumber3 = rand.Next(1, 70);
+                MegaNumber4 = rand.Next(1, 70);
+                MegaNumber5 = rand.Next(1, 70);
+                MegaNumberBonus = rand.Next(1, 25);
+            }
+            txtMega1.Text = MegaNumber1.ToString();
+            txtMega2.Text = MegaNumber2.ToString();
+            txtMega3.Text = MegaNumber3.ToString();
+            txtMega4.Text = MegaNumber4.ToString();
+            txtMega5.Text = MegaNumber5.ToString();
+            txtMegaBonus.Text = MegaNumberBonus.ToString();
+        }
+    }
+}
